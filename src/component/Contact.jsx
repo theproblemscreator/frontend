e6cfc7bc87d07aa11e06fe9b9d92e406b3732b31
Ajay <ajaygrathod5@gmail.com>
2025-08-22 09:38:25 +0530

    Gmail new added

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
     <div className="page-content">
       <h1>Contact Me</h1>
       <hr />
-      <p>Email: <a><b>ajgrathod2025@gmail.com</b></a></p>
+      <p>Email: <a><b>ajaygrathod25@gmail.com</b></a></p>
       <p>LinkedIn: <a href={linkedin_url} target="_blank" rel="noreferrer">View Profile</a></p>
     </div>
   );
